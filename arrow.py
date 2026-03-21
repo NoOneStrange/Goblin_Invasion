e6a5432e228f1e87c65ef70d7c7655001fccadb4
NoOneStrange <szymikmichal2111@gmail.com>
2026-03-21 17:12:06 +0100

    Dodanie klasy arrow i pomysłów przyszłych edycji

---
+++ arrow.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Arrow(Sprite):
+    """Tworzenie i konfiguracja strzały wystrzeliwanej przez elfa"""
+
+    def __init__(self, gi_game):
+        """Tworzy obiekt strzały w aktualnym położeniu elfa"""
+        super().__init__()
+        self.screen = gi_game.screen
+        self.settings = gi_game.settings
+        self.color = self.settings.arrow_color
+
+        #Tworzenie strzały w punkcie (0,0)
+        #następnie zdefiniowanie jej prawidłowego położenia
+        self.rect = pygame.Rect(0, 0, self.settings.arrow_width, self.settings.arrow_height)
+        self.rect.midtop = gi_game.elf.rect.midtop
+
+        self.y = float(self.rect.y)
+    
+    def update(self):
+        """Ruch strzały na ekranie"""
+        #Aktualizacja położenia strzały
+        self.y -= self.settings.arrow_spped
+        #Aktualizacjja prostokąta strzały
+        self.rect.y = self.y
+
+    def draw_arrow(self):
+        """Wyświetlanie strzały na ekranie"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
--- settings.py
+++ settings.py
@@ -11,3 +11,9 @@ class Settings:
         #Ustawienia elfa
         self.elf_speed = 1.5
 
+        #Ustawienia strzały
+        self.arrow_speed = 2.0
+        self.arrow_width = 3
+        self.arrow_height = 15
+        self.arrow_color = (60, 60, 60)
+
