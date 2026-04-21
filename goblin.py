db7a377d562eb8608a351631a120ae1bb95d4498
NoOneStrange <szymikmichal2111@gmail.com>
2026-04-21 15:28:26 +0200

    Final

--- goblin.py
+++ goblin.py
@@ -10,45 +10,8 @@ class Goblin(Sprite):
         self.screen = gi_game.screen
         self.settings = gi_game.settings
 
-        #Zaczytanie goblinów
-        self.green_goblin()
-        #self.red_goblin()
-        #self.blue_goblin()
-
-    def green_goblin(self):
-        """Utworzenie goblina zielonego (standardowa punktacja)"""
         #Wczytanie obraazu i definicja atrybutu rect
         self.image = pygame.image.load('images/goblin_green.png')
-        w, h = self.image.get_size()
-        self.image = pygame.transform.scale(self.image, (w / 4, h / 4))
-        self.rect = self.image.get_rect()
-
-        #Umieszczenie goblina w pobliżu lewego górnego rogu
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
-
-        #Przechowywanie dokładnego poziomego położenia goblina
-        self.x = float(self.rect.x)
-    
-    def red_goblin(self):
-        """Utworzenie goblina czerwonego (standardowa punktacja pomnożona przez 2)"""
-        #Wczytanie obraazu i definicja atrybutu rect
-        self.image = pygame.image.load('images/goblin_red.png')
-        w, h = self.image.get_size()
-        self.image = pygame.transform.scale(self.image, (w / 4, h / 4))
-        self.rect = self.image.get_rect()
-
-        #Umieszczenie goblina w pobliżu lewego górnego rogu
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
-
-        #Przechowywanie dokładnego poziomego położenia goblina
-        self.x = float(self.rect.x)
-    
-    def blue_goblin(self):
-        """Utworzenie goblina niebieskiego (standardowa punktacja pomnożona przez 4)"""
-        #Wczytanie obraazu i definicja atrybutu rect
-        self.image = pygame.image.load('images/goblin_blue.png')
         w, h = self.image.get_size()
         self.image = pygame.transform.scale(self.image, (w / 4, h / 4))
         self.rect = self.image.get_rect()
